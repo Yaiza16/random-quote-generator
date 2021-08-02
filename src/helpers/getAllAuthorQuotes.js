2b60f60508d63075d9626d0ac0639d1a501a39cc
Yaiza <yaizavallejo16@gmail.com>
2021-08-02 15:33:03 +0200

    Update: add second request once number of quotes per author is known

--- src/helpers/getAllAuthorQuotes.js
+++ src/helpers/getAllAuthorQuotes.js
@@ -2,10 +2,16 @@ const getAllAuthorQuotes = async (authorName, setLoading) =>{
     // Set loader
     setLoading(true)
 
-    const url = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${authorName}&limit=3`
+    const url = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${authorName}`
     const res = await fetch(url)
     const data = await res.json();
-    const newQuote = await data.data;
+    const numberOfQuotes = await data.totalQuotes;
+    console.log(data.totalQuotes)
+
+    const newUrl = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${authorName}&limit=${numberOfQuotes}`
+    const newRes = await fetch(newUrl)
+    const newData = await newRes.json();
+    const newQuote = await newData.data
 
     const allQuotes = newQuote.map(quote =>(
         {
@@ -22,3 +28,31 @@ const getAllAuthorQuotes = async (authorName, setLoading) =>{
 }
 
 export default getAllAuthorQuotes;
+
+
+// const getAllAuthorQuotes = async (authorName, setLoading) =>{
+//     // Set loader
+//     setLoading(true)
+
+//     const url = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${authorName}`
+//     const res = await fetch(url)
+//     const data = await res.json();
+//     const newQuote = await data.data;
+//     console.log(data.totalQuotes)
+
+//     const allQuotes = newQuote.map(quote =>(
+//         {
+//             id: quote._id,
+//             author: quote.quoteAuthor,
+//             genre: quote.quoteGenre,
+//             text: quote.quoteText
+//         }
+//     ))
+
+//     //Set loader
+//     setLoading(false)
+//     return allQuotes
+// }
+
+// export default getAllAuthorQuotes;
+
