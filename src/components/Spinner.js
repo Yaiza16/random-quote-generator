800dd0919f4619e76a1b529c6a93a9eb5a0a37e3
Yaiza <yaizavallejo16@gmail.com>
2021-08-03 17:09:56 +0200

    update: format code

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import './Spinner.css'
+import React from "react";
+import "./Spinner.css";
 
-const Spinner = () =>{
-    return(
-        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-    )
-}
-export default Spinner
+const Spinner = () => {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Spinner;
