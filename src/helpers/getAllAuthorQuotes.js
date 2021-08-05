4d37cbf60726aa61196792ed0667e479f05506c9
Yaiza <yaizavallejo16@gmail.com>
2021-08-05 09:43:03 +0200

    Update: delete comments

--- src/helpers/getAllAuthorQuotes.js
+++ src/helpers/getAllAuthorQuotes.js
@@ -1,21 +1,10 @@
 const getAllAuthorQuotes = async (authorName, setLoading, pageNumber) =>{
     // Set loader
     setLoading(true)
-    // console.log('Page number:' + pageNumber)
+    
     const url = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${authorName}&limit=3&page=${pageNumber}`
     const res = await fetch(url)
     const data = await res.json();
-    // const newQuote = await data.data;
-    // console.log(data.totalQuotes)
-
-    // const allQuotes = newQuote.map(quote =>(
-    //     {
-    //         id: quote._id,
-    //         author: quote.quoteAuthor,
-    //         genre: quote.quoteGenre,
-    //         text: quote.quoteText
-    //     }
-    // ))
 
     //Set loader
     setLoading(false)
@@ -23,31 +12,3 @@ const getAllAuthorQuotes = async (authorName, setLoading, pageNumber) =>{
 }
 
 export default getAllAuthorQuotes;
-
-
-// const getAllAuthorQuotes = async (authorName, setLoading) =>{
-//     // Set loader
-//     setLoading(true)
-
-//     const url = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${authorName}`
-//     const res = await fetch(url)
-//     const data = await res.json();
-//     const newQuote = await data.data;
-//     console.log(data.totalQuotes)
-
-//     const allQuotes = newQuote.map(quote =>(
-//         {
-//             id: quote._id,
-//             author: quote.quoteAuthor,
-//             genre: quote.quoteGenre,
-//             text: quote.quoteText
-//         }
-//     ))
-
-//     //Set loader
-//     setLoading(false)
-//     return allQuotes
-// }
-
-// export default getAllAuthorQuotes;
-
